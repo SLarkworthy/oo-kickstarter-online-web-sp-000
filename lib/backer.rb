4bfdcce4e75e79a2f71acb2479809f9a551bcd42
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-17 21:12:57 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,8 +9,8 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    if !project.backed_projects.include?(self)
-      backer.back_project(self)
+    if !project.backers.include?(self)
+      project.back_project(self)
   end
   
 
